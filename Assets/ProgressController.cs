643c31aeabe9f2a7cebc4f7b97129a80ec387e25
hideharu suyama <h_suya@hotmail.com>
2017-05-17 01:01:37 +0900

    first commit

---
+++ Assets/ProgressController.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ProgressController : MonoBehaviour {
+
+	[Serializable]
+	public class UpdateEvent: UnityEvent<float> { }
+	public UpdateEvent onValueChanged = new UpdateEvent();
+	public UnityEvent onComplete = new UnityEvent();
+
+	bool busy = false;
+	public float rate
+	{
+		get; protected set;
+	}
+
+	List<Handle> mHandleList = new List<Handle>();
+	public List<Handle> handleList { get { return mHandleList; } }
+	int mNextProcess = 0;
+	int nextProcess { get { return mNextProcess; } }
+	public int processCount { get { return mHandleList.Count; } }
+	public int processMax = 1;
+
+	int mBusyCount = 0;
+	int mCompleteCount = 0;
+	public int completeCount { get { return mCompleteCount; } }
+	public bool completed
+	{
+		get
+		{
+			return mCompleteCount == mHandleList.Count;
+		}
+	}
+
+
+	float startTime;
+	float endTime;
+	public float time {
+		get
+		{
+			if (completed) {
+				return endTime - startTime;
+			}
+			if (busy) {
+				return Handle.GetTime() - startTime;
+			}
+			return 0.0f;
+		}
+	}
+
+
+	void Start()
+	{
+		Initialize();
+	}
+
+	public void Initialize()
+	{
+		busy = false;
+		rate = 0.0f;
+		mHandleList.Clear();
+		mNextProcess = 0;
+		mBusyCount = 0;
+		mCompleteCount = 0;
+	}
+
+	public delegate IEnumerator ProcessDelegate(Handle handle);
+
+	public Handle EntryProcess(float predictedTime, ProcessDelegate processDelegate)
+	{
+		var handle = new Handle();
+		handle.name = mHandleList.Count().ToString();
+		handle.controller = this;
+		handle.predictedTime = predictedTime;
+		handle.process = processDelegate;
+		mHandleList.Add(handle);
+		return handle;
+	}
+
+	public void StartProcess()
+	{
+		startTime = Handle.GetTime();
+		busy = true;
+		while (mBusyCount < processMax) {
+			StartNextProcess();
+			if (completed) {
+				Complete();
+				break;
+			}
+		}
+	}
+	void StartNextProcess()
+	{
+		mBusyCount++;
+		var startIndex = mNextProcess++;
+		mHandleList[startIndex].Start();
+
+	}
+	internal void CompleteProcess(Handle handle)
+	{
+		mBusyCount--;
+		mCompleteCount++;
+		if (completed) {
+			Complete();
+			return;
+		}
+		if (mNextProcess < handleList.Count) {
+			StartNextProcess();
+		}
+	}
+	void Complete()
+	{
+		endTime = Handle.GetTime();
+		onComplete.Invoke();
+	}
+
+	void Update()
+	{
+		if (busy) {
+			UpdateRate();
+			if (completed) {
+				busy = false;
+			}
+		}
+	}
+	void UpdateRate()
+	{
+		float sumRate = 0;
+		float sumAll = 0;
+		foreach (var handle in mHandleList) {
+			var rate = customizeRateDelegate(handle);
+			if (1.0f < rate) {
+				rate = 1.0f;
+			}
+			if (rate < handle.rate) {
+				rate = handle.rate;
+			}
+			sumRate += rate * handle.predictedTime;
+			sumAll += handle.predictedTime;
+		}
+		var newRate = sumRate / sumAll;
+		if (rate < newRate) {
+			rate = newRate;
+			if (onValueChanged != null) {
+				onValueChanged.Invoke(rate);
+			}
+		}
+	}
+	public delegate float CustomizeRateDelegate(Handle handle);
+	public CustomizeRateDelegate customizeRateDelegate = CustomizeRate;
+	static float CustomizeRate(Handle handle)
+	{
+		var predictedRate = handle.GetPredictedRate();
+
+		var limitter = 0.98f;
+		if (limitter < predictedRate) {
+			predictedRate = limitter;
+		}
+		return predictedRate;
+	}
+
+
+
+	public class Handle {
+		public ProgressController controller { get; internal set; }
+
+		public enum State {
+			Idle,
+			Started,
+			Compeleted
+		}
+		State mState = State.Idle;
+		public State state
+		{
+			get { return mState; }
+		}
+
+		public string name;
+		public bool started { get { return State.Started <= state; } }
+		public bool completed { get { return state == State.Compeleted; } }
+		public float startTime { get; protected set; }
+		public float endTime { get; protected set; }
+		public float processTime
+		{
+			get
+			{
+				if (state == State.Started) {
+					return GetTime() - startTime;
+				}
+				if (state == State.Compeleted) {
+					return endTime - startTime;
+				}
+				return 0.0f;
+			}
+		}
+		public float predictedTime = 1.0f;
+		public float rate;
+		internal ProcessDelegate process;
+
+		public static float GetTime()
+		{
+			return Time.realtimeSinceStartup;
+		}
+		internal void Start()
+		{
+			mState = State.Started;
+			startTime = GetTime();
+			controller.StartCoroutine(ProcessCoroutine());
+		}
+		IEnumerator  ProcessCoroutine()
+		{
+			yield return process(this);
+
+			endTime = GetTime();
+			rate = 1.0f;
+			mState = State.Compeleted;
+			controller.CompleteProcess(this);
+		}
+
+		public float GetPredictedRate()
+		{
+			if (!started) {
+				return 0;
+			}
+			if (completed) {
+				return 1.0f;
+			}
+			var time = GetTime();
+			float predictedRate = (time - startTime) / predictedTime;
+			if (1.0f < predictedRate) {
+				return 1.0f;
+			}
+			return predictedRate;
+
+		}
+	}
+}
---
+++ Assets/sample.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class sample : MonoBehaviour {
+
+
+	public ProgressController controller;
+	public Image image;
+	public Text text;
+
+	void Start () {
+		mStateAction = State_Setup;
+		controller = GetComponentInChildren<ProgressController>();
+		controller.Initialize();
+		image = GetComponentInChildren<Image>();
+		text = GetComponentInChildren<Text>();
+	}
+	
+
+	public int processCount = 10;
+	public int processMax = 2;
+	public float predicteEndTime = 1.0f;
+	public float processEndTime = 2.0f;
+
+	public enum UpdateType {
+		Liner,
+		Step
+	}
+	public UpdateType updateType = UpdateType.Step;
+
+	Action mStateAction;
+
+	void OnGUI()
+	{
+		if (mStateAction != null) {
+			mStateAction();
+		}
+	}
+
+	void State_Setup()
+	{
+		GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("総プロセス数:" + processCount.ToString("D2"));
+		processCount = (int)GUILayout.HorizontalSlider(processCount, 1, 15);
+		GUILayout.EndHorizontal();
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("同時処理プロセス数:" + processMax.ToString("D2"));
+		processMax = (int)GUILayout.HorizontalSlider(processMax, 1, 3);
+		GUILayout.EndHorizontal();
+
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("１プロセスの予測終了時間:" + predicteEndTime.ToString("F1"));
+		predicteEndTime = GUILayout.HorizontalSlider(predicteEndTime, 0.1f, 10);
+		GUILayout.EndHorizontal();
+
+		GUILayout.BeginHorizontal();
+		GUILayout.Label("１プロセスの実終了時間:" + processEndTime.ToString("F1"));
+		processEndTime = GUILayout.HorizontalSlider(processEndTime, 0.1f, 10);
+		GUILayout.EndHorizontal();
+
+		if (GUILayout.Button("Start")) {
+			mStateAction = State_Processing;
+
+			//初期化
+			controller.Initialize();
+			//プロセスの登録
+			for (int i = 0; i < processCount; i++) {
+				if (updateType == UpdateType.Step) {
+					//進捗状況を随時報告
+					controller.EntryProcess(predicteEndTime, SampleStep);
+				}
+				else {
+					//終了時に完了のみ報告
+					controller.EntryProcess(predicteEndTime, SampleLiner);
+				}
+			}
+			//進捗状況の受取
+			controller.onValueChanged.AddListener(rate =>
+			{
+				image.fillAmount = rate;
+				text.text = (rate * 100).ToString("F1") + "%";
+			});
+			//完了通知の受取
+			controller.onComplete.AddListener(() =>
+			{
+				mStateAction = State_Completed;
+			});
+			//同時処理プロセス数の設定
+			controller.processMax = processMax;
+			//プロセスを登録順に実行開始
+			controller.StartProcess();
+		}
+
+		GUILayout.EndArea();
+
+	}
+
+	//進捗状況を随時報告
+	IEnumerator SampleLiner(ProgressController.Handle handle)
+	{
+		var startTime = ProgressController.Handle.GetTime();
+		do {
+			yield return null;
+			var time = ProgressController.Handle.GetTime();
+			var rate = (time - startTime) / processEndTime;
+			if (1.0f < rate) {
+				rate = 1.0f;
+			}
+			handle.rate = rate;
+		} while (handle.rate < 1.0f);
+	}
+
+	//終了時に完了のみ報告
+	IEnumerator SampleStep(ProgressController.Handle handle)
+	{
+		yield return new WaitForSeconds(processEndTime);
+		handle.rate = 1.0f;
+	}
+
+
+	void State_Processing()
+	{
+		EditorGUILayout.LabelField("time:" + controller.time.ToString("F1") + " sec");
+		EditorGUILayout.LabelField(string.Format("{0}/{1}", controller.completeCount, controller.processCount));
+	}
+	void State_Completed()
+	{
+		if (GUILayout.Button("Restart")) {
+			controller.Initialize();
+			mStateAction = State_Setup;
+		}
+		EditorGUILayout.LabelField("Result");
+		EditorGUILayout.LabelField("total:" + controller.time.ToString("F1") + " sec");
+		foreach (var handle in controller.handleList) {
+			EditorGUILayout.LabelField(handle.name + ":" + handle.processTime.ToString("F1") + " sec");
+
+		}
+	}
+}
